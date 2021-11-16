ecb6555c4ce7e398d46e55efadb4f1f8de2ca8d5
Emma Grey <emgrey02@gmail.com>
2021-11-15 22:12:07 -0500

    Make resistor function properly

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-import { getResistorOhms } from './resistor.js';
+import { getResistorOhms } from "./resistor.js";
+
+//buttons
+const allButtons = document.querySelectorAll(".box");
+const firstBandColor = document.querySelector("#b0");
+const secondBandColor = document.querySelector("#b1");
+const multiplierBandColor = document.querySelector("#b2");
+const toleranceBandColor = document.querySelector("#b3");
+
+allButtons.forEach((button) => {
+  button.addEventListener("click", changeColor);
+  button.addEventListener("click", calculateValue);
+});
+
+function changeColor(e) {
+  const selectedColor = e.target.innerText.toLowerCase();
+  const currentBand = e.target.parentElement.id;
+
+  if (currentBand === "color0") {
+    firstBandColor.className = `band ${selectedColor}`;
+  } else if (currentBand === "color1") {
+    secondBandColor.className = `band ${selectedColor}`;
+  } else if (currentBand === "color2") {
+    multiplierBandColor.className = `band ${selectedColor}`;
+  } else if (currentBand === "color3") {
+    toleranceBandColor.className = `band ${selectedColor}`;
+  }
+}
+
+function calculateValue(e) {
+  const resistorValue = document.querySelector(".answer");
+
+  const text = getResistorOhms({
+    color1: `${firstBandColor.classList[1]}`,
+    color2: `${secondBandColor.classList[1]}`,
+    multiplier: `${multiplierBandColor.classList[1]}`,
+    tolerance: `${toleranceBandColor.classList[1]}`,
+  });
+
+  resistorValue.innerText = text;
+}
--- resistor.js
+++ resistor.js
@@ -9,6 +9,124 @@
    http://bit.ly/2NjS274
 */
 
-const getResistorOhms = (bands) => {};
+function getColorValue(color) {
+  const colorCodes = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+  return colorCodes[color];
+}
+
+/**
+ * Returns the number of the multiplier
+ * @param {string} color - the color of the multiplier bands
+ * @returns {number} - the multiplier number
+ */
+function getMultiplierValue(color) {
+  const multiplierCodes = {
+    black: 1,
+    brown: 10,
+    red: 100,
+    orange: 1000,
+    yellow: 10000,
+    green: 100000,
+    blue: 1000000,
+    violet: 10000000,
+    grey: 100000000,
+    white: 1000000000,
+    gold: 0.1,
+    silver: 0.01,
+  };
+  return multiplierCodes[color];
+}
+
+/**
+ * Returns the value of the first 3 bands of the resistor colors
+ * @param {object} bands - an object with 2 keys
+ * @param {string} bands.color1 - the first color
+ * @param {string} bands.color2 - the second color
+ * @param {string} bands.multiplier - the multiplier color
+ * @returns {number} - representing the value of the first 3 bands of the resistor
+ */
+function getThreeBandValue(bands) {
+  const first = getColorValue(bands.color1).toString();
+  const second = getColorValue(bands.color2).toString();
+  const together = +(first + second);
+  const total = together * getMultiplierValue(bands.multiplier);
+  if (bands.multiplier === "gold") {
+    return +total.toFixed(1);
+  }
+  if (bands.multiplier === "silver") {
+    return +total.toFixed(2);
+  }
+  return total;
+}
+
+/**
+ * Returns a string representation of the value with metric notation
+ * @param {number} val - number representing the value of the resistor
+ * @returns {string} - a string representation of the numeric value with metric notation
+ */
+function formatNumber(val) {
+  const valString = val.toString();
+  const valLength = valString.length;
+
+  if (valLength <= 3) {
+    return valString;
+  }
+  if (valLength > 3 && valLength <= 6) {
+    return `${(val / 1000).toString()}k`;
+  }
+  if (valLength > 6 && valLength <= 9) {
+    return `${(val / 1000000).toString()}M`;
+  }
+  if (valLength > 9 && valLength <= 12) {
+    return `${(val / 1000000000).toString()}G`;
+  }
+  return undefined;
+}
+
+/**
+ * Returns the tolerance of the resistor according to its color value
+ * @param {string} color - the color of the tolerance band to
+ * @returns {string} - the tolerance value in percent using the ± symbol
+ */
+function getTolerance(color) {
+  const colorTolerance = {
+    brown: "±1%",
+    red: "±2%",
+    green: "±0.5%",
+    blue: "±0.25%",
+    violet: "±0.1%",
+    grey: "±0.05%",
+    gold: "±5%",
+    silver: "±10%",
+  };
+  return colorTolerance[color];
+}
+
+/**
+ *
+ * @param {object} bands - the object with the 4 bands
+ * @param {string} bands.color1 - the first color
+ * @param {string} bands.color2 - the second color
+ * @param {string} bands.multiplier - the multiplier color
+ * @param {string} bands.tolerance - the tolerance color
+ * @returns {string} - a string representation of the resistor value according to
+ */
+function getResistorOhms(bands) {
+  const resistorValue = formatNumber(getThreeBandValue(bands));
+  return `Resistor value: ${resistorValue} Ohms ${getTolerance(
+    bands.tolerance
+  )}`;
+}
 
 export { getResistorOhms };
